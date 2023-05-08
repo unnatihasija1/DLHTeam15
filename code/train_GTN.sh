836ab70f4e2e1985d50cd53674ab7d2af3576271
unnatihasija1 <109382284+unnatihasija1@users.noreply.github.com>
2023-05-08 15:33:24 -0400

    Update train_GTN.sh

--- code/train_GTN.sh
+++ code/train_GTN.sh
@@ -2,7 +2,7 @@
 python run_pretraining.py --model_name GBert-pretraining-qGTN1 --num_train_epochs 5 --do_train --graph
 python run_gbert.py --model_name GBert-predict-qGTN1 --use_pretrain --pretrain_dir ../saved/GBert-pretraining-qGTN1 --num_train_epochs 5 --do_train --graph
 
-for i in {1..15}
+for i in {1..6}
 do
 python run_pretraining.py --model_name GBert-pretraining-qGTN1 --use_pretrain --pretrain_dir ../saved/GBert-predict-qGTN1 --num_train_epochs 5 --do_train --graph
 python run_gbert.py --model_name GBert-predict-qGTN1 --use_pretrain --pretrain_dir ../saved/GBert-pretraining-qGTN1 --num_train_epochs 5 --do_train --graph
